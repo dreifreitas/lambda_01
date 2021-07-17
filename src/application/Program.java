7058ba59dd0a8f3b25eb49685787be4f62ee5a97
Andrei <westerlanio@gmail.com>
2021-07-17 20:16:37 -0300

    Comparator obj de expressão lambda sem chaves

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,9 +22,8 @@ public class Program {
         products.add(pdt2);
         products.add(pdt3);
 
-        Comparator<Product> comp = (p1, p2) -> {
-            return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-        };
+        Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+        
 
         products.sort(comp);
 
